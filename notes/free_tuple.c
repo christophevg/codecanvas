0eae2b23c0c885b73161cfbf9ecbd50384547344
Christophe VG <contact@christophe.vg>
2014-03-18 20:46:45 +0100

    added more info to show reuse of memory space

--- notes/free_tuple.c
+++ notes/free_tuple.c
@@ -18,13 +18,14 @@ typedef struct payload_t {
 payload_t* make_payload(uint8_t* data, int size) {
   payload_t* payload = malloc(sizeof(payload_t));
   payload->size = size;
-  payload->data = malloc(size*sizeof(uint8_t));
+  payload->data = malloc(size * sizeof(uint8_t));
   memcpy(payload->data, data, size);
   return payload;
 }
 
 payload_t* copy_payload(payload_t* source) {
   payload_t* copy = malloc(sizeof(payload_t));
+  copy->size = source->size;
   copy->data = malloc(source->size * sizeof(uint8_t));
   memcpy(copy->data, source->data, source->size);
   return copy;
@@ -59,45 +60,49 @@ int main(void) {
   
   payload_t* payload = make_payload(data, 1024);
 
-  printf("data    : %lu\n", malloc_size(data));               // 1024
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
   free(data);
-  printf("data    : %lu\n", malloc_size(data));               // 0
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
+  
+  uint8_t* more = malloc(1024*sizeof(uint8_t));
+  more[1023] = 0x30;
+  printf("more    (%p) : %lu\n", more, malloc_size(more));
   
   tuple_t* tuple1 = make_tuple(1, payload);
 
-  printf("data    : %lu\n", malloc_size(data));               // 1024 ??
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
   printf("\n");
 
   tuple_t* tuple2 = make_tuple(2, payload);
 
-  printf("data    : %lu\n", malloc_size(data));               // 1024 ??
-  printf("payload : %lu\n", malloc_size(payload->data));
-  printf("tuple1  : %lu\n", malloc_size(tuple1->payload->data));
-  printf("tuple2  : %lu\n", malloc_size(tuple2->payload->data));
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
+  printf("payload (%p) : %lu\n", payload, malloc_size(payload->data));
+  printf("tuple1  (%p) : %lu\n", tuple1->payload->data, malloc_size(tuple1->payload->data));
+  printf("tuple2  (%p) : %lu\n", tuple2->payload->data, malloc_size(tuple2->payload->data));
   printf("\n");
 
   free_payload(payload);
 
-  printf("data    : %lu\n", malloc_size(data));               // 1024 ??
-  printf("payload : %lu\n", malloc_size(payload->data));
-  printf("tuple1  : %lu\n", malloc_size(tuple1->payload->data));
-  printf("tuple2  : %lu\n", malloc_size(tuple2->payload->data));
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
+  printf("payload (%p) : %lu\n", payload, malloc_size(payload->data));
+  printf("tuple1  (%p) : %lu\n", tuple1->payload->data, malloc_size(tuple1->payload->data));
+  printf("tuple2  (%p) : %lu\n", tuple2->payload->data, malloc_size(tuple2->payload->data));
   printf("\n");
 
   free_tuple(tuple1);
 
-  printf("data    : %lu\n", malloc_size(data));               // 0 !!!
-  printf("payload : %lu\n", malloc_size(payload->data));
-  printf("tuple1  : %lu\n", malloc_size(tuple1->payload->data));
-  printf("tuple2  : %lu\n", malloc_size(tuple2->payload->data));
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
+  printf("payload (%p) : %lu\n", payload, malloc_size(payload->data));
+  printf("tuple1  (%p) : %lu\n", tuple1->payload->data, malloc_size(tuple1->payload->data));
+  printf("tuple2  (%p) : %lu\n", tuple2->payload->data, malloc_size(tuple2->payload->data));
   printf("\n");
 
   free_tuple(tuple2);
 
-  printf("data    : %lu\n", malloc_size(data));
-  printf("payload : %lu\n", malloc_size(payload->data));
-  printf("tuple1  : %lu\n", malloc_size(tuple1->payload->data));
-  printf("tuple2  : %lu\n", malloc_size(tuple2->payload->data));
+  printf("data    (%p) : %lu\n", data, malloc_size(data));
+  printf("payload (%p) : %lu\n", payload, malloc_size(payload->data));
+  printf("tuple1  (%p) : %lu\n", tuple1->payload->data, malloc_size(tuple1->payload));
+  printf("tuple2  (%p) : %lu\n", tuple2->payload->data, malloc_size(tuple2->payload));
   printf("\n");
 
   exit(EXIT_SUCCESS);
